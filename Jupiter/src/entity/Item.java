49cc13cdb2679b04a0c48b412b4da0a59e09004d
weidong6686 <weidong6686@gmail.com>
2019-07-19 09:58:21 -0700

    initial project

---
+++ Jupiter/src/entity/Item.java
@@ -0,0 +1,132 @@
+package entity;
+
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item {
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+	
+	/**
+	 * This is a builder pattern in Java. 构架time工具对象
+	 */
+	// private为了封装性更好，利用builder功能赋值;
+	// 使用itembBuilder 赋值 item对象多功能， 因为java没有default输入，使用design pattern输入
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.rating = builder.rating;
+		this.address = builder.address;
+		this.categories = builder.categories;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+		this.distance = builder.distance;
+	} 
+	
+	public String getItemId() {
+		return itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public Set<String> getCategories() {
+		return categories;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("item_id", itemId);
+			obj.put("name", name);
+			obj.put("rating", rating);
+			obj.put("address", address);
+			obj.put("categories", new JSONArray(categories));
+			obj.put("image_url", imageUrl);
+			obj.put("url", url);
+			obj.put("distance", distance);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+
+	// inner class内部类： static是必须的；同时间只能new一个子类对象，用于赋值。先要newBuilder。
+	//但是没有static每次要实例化外类，再实例内部类。
+	public static class ItemBuilder {
+	
+		private String itemId;
+		private String name;
+		private double rating;
+		private String address;
+		private Set<String> categories;
+		private String imageUrl;
+		private String url;
+		private double distance;
+		
+		public ItemBuilder setItemId(String itemId) {
+			this.itemId = itemId;
+			return this;
+		}
+		public ItemBuilder setName(String name) {
+			this.name = name;
+			return this;
+		}
+		public ItemBuilder setRating(double rating) {
+			this.rating = rating;
+			return this;
+		}
+		public ItemBuilder setAddress(String address) {
+			this.address = address;
+			return this;
+		}
+		public ItemBuilder setCategories(Set<String> categories) {
+			this.categories = categories;
+			return this;
+		}
+		public ItemBuilder setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+			return this;
+		}
+		public ItemBuilder setUrl(String url) {
+			this.url = url;
+			return this;
+		}
+		public ItemBuilder setDistance(double distance) {
+			this.distance = distance;
+			return this;
+		}
+		//表示set结束，返回一个item对象
+		public Item build() {
+			return new Item(this); 
+		}
+	}
+//	//@test
+//	public static void main() {
+//		Item item = new Item.ItemBuilder().setAddress("abc").setName("jack").build();
+//	}
+
+}
